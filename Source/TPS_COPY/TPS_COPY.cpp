427b0c2de999e6dc66166b275ca608b14ef75441
won <wonheo7527@gmail.com>
2023-01-10 16:20:55 +0900

    Initial commit

---
+++ Source/TPS_COPY/TPS_COPY.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TPS_COPY.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPS_COPY, "TPS_COPY" );
---
+++ Source/TPS_COPY/TPS_COPYGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPS_COPYGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_COPY_API ATPS_COPYGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
